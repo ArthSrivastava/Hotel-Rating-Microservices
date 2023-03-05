fd0a7943b739ea557efa93948f706a6ed6e5375c
Arth <arth4701@gmail.com>
2023-03-05 23:59:12 +0530

    ratelimiter and retry support added

--- UserService/src/main/java/com/userservice/userservice/controllers/UserController.java
+++ UserService/src/main/java/com/userservice/userservice/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.userservice.userservice.controllers;
 import com.userservice.userservice.entities.User;
 import com.userservice.userservice.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -36,9 +38,14 @@ public class UserController {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    int retryCount = 1;
+
     @GetMapping("/{id}")
-    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "hotelRatingBreakerFallback")
+//    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "hotelRatingBreakerFallback")
+//    @Retry(name = "ratingHotelService", fallbackMethod = "hotelRatingBreakerFallback")
+    @RateLimiter(name = "userServiceLimiter", fallbackMethod = "hotelRatingBreakerFallback")
     public ResponseEntity<User> getUserById(@PathVariable String id) {
+        logger.info("Retry count: {}", retryCount++);
         User userById = userService.getUserById(id);
         return ResponseEntity.ok(userById);
     }
